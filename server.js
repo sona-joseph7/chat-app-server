1598704439d897f026513ecbe0cfae9ff9ac8605
sona-joseph7 <sonajosephsj07@gmail.com>
2025-02-01 20:22:03 +0530

    fourth commit

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const http = require('http')
 const {Server} = require('socket.io')
 const cors = require('cors')
+const app = express()
+
 app.use(cors({
   origin:'https://r-e-a-l-t-i-m-e-chat-app.netlify.app'
 }))
-const app = express()
+
 const server = http.createServer(app)
 const io = new Server(server, {
   cors:{
